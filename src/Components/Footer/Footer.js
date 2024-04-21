44b6dc5cd49d38e05796c39ef432b9563d1e8dd3
krishna1326 <krishnamoorthym171@gmail.com>
2024-04-21 13:47:49 -0700

    footer and testimonial

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import './Footer.css'
+
+
+
+const Footer = () => {
+    return (
+        <div className='footer container'>
+            <div className='row-1'>
+                <div className="about footer-col">
+                    <h4 className='heading'>FoodieWeb</h4>
+                    <div className='content'>
+                        <p>Indulge in a symphony of flavors, where each plate is a canvas for culinary excellence.</p>
+                    </div>
+                </div>
+
+                <div className='link footer-col'>
+                    
+                    <h4 className='heading'>Links</h4>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Menu</a></li>
+                        <li><a href="/">Testimonial</a></li>
+                        <li><a href="/">Contact Us</a></li>
+                    </ul>
+                    
+                    
+                </div>
+
+                <div className='menu footer-col'>
+                    <h4>Menu</h4>
+                    <ul>
+                       <li><a href="/">Our Dishes</a></li>
+                       <li><a href="/">Premium Menu</a></li>
+                    </ul>
+                </div>
+
+                <div className='footer-col'>
+                    <h4 className='heading'>Contact Us</h4>
+                    <ul>
+                        <li><a href="/">Foodie@gmail.com</a></li>
+                        <li><a href="/">+64 7708171867</a></li>
+                        <li><a href="/">Social Media</a></li>
+                    </ul>
+                </div>
+            </div>
+
+
+
+            <div className='copy row-2'>
+                <p>
+                &copy;copyright developed by <span>guru mess</span> | All rights reserved
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
