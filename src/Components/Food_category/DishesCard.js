35bfc6fa0b128813462fd0e7785c9f9c00b3c24d
kiruthikaor <132900132+kiruthikaor@users.noreply.github.com>
2024-04-23 15:34:37 -1100

    css

---
+++ src/Components/Food_category/DishesCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BsStartFill } from "react-icons/bs"
+import { BsHalfFill } from "react-icons/bs"
+
+const DishesCard = () => {
+  return (
+    <div>DishesCard</div>
+  )
+}
+
+export default DishesCard
